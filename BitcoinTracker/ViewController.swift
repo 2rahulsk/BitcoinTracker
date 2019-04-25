1b9607d85be1473fccdd4d17864ef6ca18d6c29a
2rahulsk <30774426+2rak@users.noreply.github.com>
2019-04-25 20:36:19 +1000

    initial commit

--- BitcoinTracker/ViewController.swift
+++ BitcoinTracker/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    @IBOutlet weak var priceValue: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/constants/exchangerates/global"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    var json : JSON = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if json == "" {
+            SVProgressHUD.show(withStatus: "Loading")
+            getBitcoinPrice()
+        }
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        
+        return currencyArray[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let currency = currencyArray[row]
+        let value = json["rates"][currency]["rate"].stringValue
+        priceValue.text = value
+        
+    }
+    
+    //    //MARK: - Networking
+    //    /***************************************************************/
+    
+    func getBitcoinPrice() {
+        
+        Alamofire.request(baseURL).responseJSON {
+            response in
+            
+            if response.result.isSuccess {
+                self.json = JSON(response.result.value!)
+                print(self.json)
+                SVProgressHUD.dismiss()
+                
+            } else {
+                
+                print("Something went wrong")
+            }
+        }
+        
     }
 
+    
 
 }
 
